d058169295f37b4ea4f1d180788856fcf728a46f
ayeshamaryam <ayeshamaryam484@gmail.com>
2022-09-09 18:57:06 +0530

    second commit

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,9 +1,28 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  test "post should be valid" do 
-    @post = Post.create(title: "yfgcfcf", content: "post testing in testing file",users.email: "ayesha@example.com")
-     assert @post.valid?
-   end
+  test "user must exits" do 
+  @post = post.create(title: "my first title", content: "my first app using testing")
+   assert @post.valid?
+end
 
+test "title must exits" do 
+  @post = post.create(title: " ", content: "my first app using testing")
+   assert @post.valid?
+end
+
+test "title must not be too short" do 
+  @post = post.create(title: "sdf", content: "my first app using testing")
+   assert @post.valid?
+end
+
+test "content must exists" do 
+  @post = post.create(title: "my first title", content: "")
+   assert @post.valid?
+end
+
+test "content must not be too short" do 
+  @post = post.create(title: "my first title", content: "my")
+   assert @post.valid?
+end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,5 +1,8 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  
+  test "user must logged in" do 
+    user.email = "ayesha@example.com"
+    user.password = "ayesha"
+  end
 end
